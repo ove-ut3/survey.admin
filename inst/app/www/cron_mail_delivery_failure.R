24f87eb48342eb7a34a76363c65c305b57b51dac
Stéphane Le Hénanff <github@le-henanff.fr>
2019-12-13 11:01:53 +0100

    Added inst folder

---
+++ inst/app/www/cron_mail_delivery_failure.R
@@ -0,0 +1,7 @@
+survey.admin::mail_delivery_failure(
+  sqlite_base = "/home/shiny/enquete-ip.sqlite", 
+  imap_server = "imaps://webmail.univ-tlse3.fr",
+  username = "lhs7648a",
+  password = "Jhceljam16a.",
+  mailbox = "ove/Inbox"
+)
---
+++ inst/app/www/cron_suivi.R
@@ -0,0 +1,1 @@
+survey.admin::cron_suivi(sqlite_base = "/home/shiny/enquete-ip.sqlite")
